67adec0d17e14c6eb6e17322f414ed928aea5b45
Yousefbardisy <49583908+Yousefbardisy@users.noreply.github.com>
2021-11-10 00:07:00 +0200

    backend edit

--- Back-End/models/user.js
+++ Back-End/models/user.js
@@ -1,26 +1,35 @@
 const mongoose = require('mongoose');
+const validator = require("validator");
+
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, "Please provide your name"],
+        maxlength: [50, "Name must not exceed 50 characters"],
+        minlength: [2, "Name must not be less than 2 characters"],
+        trim: true,
+      
     },
     email: {
         type: String,
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide a valid email"],
     },
    
     phone: {
         type: String,
-        required: true,
-    },
+        required: [true, "You must have a phone"],
+        validate(value){
+            if(!validator.isMobilePhone(value, ['ar-EG'])) throw new Error('egyption mobile needed')
+        }       },
     type: {
         type: String,
-        required: true,
-        enum: ["Property Insurance","life Insurance","travel Insurance's","car Insurance","medical Insurance"]
+        required: [true," dif type"],
+        enum: ["Property Insurance","life Insurance","travel Insurance's","car Insurance","medical Insurance individual","medical Insurance company","customer service"]
     },
     message: {
         type: String,
-        required: true,
     },
 
     
